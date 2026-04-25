153fd7a6936f4f9c6e79e9550d828e831cfbd492
[dyad] <gunjarigourav@gmail.com>
2026-04-25 13:56:25 +0530

    [dyad] Fixing Three.js undefined error and moving 3D assets - wrote 1 file(s), renamed 6 file(s)

--- src/components/phoenix-model.tsx
+++ src/components/phoenix-model.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React, { Suspense } from "react";
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
 function Model(props: any) {
+  // The model is now in the public directory
   const { scene } = useGLTF("/3dmodels/phoenix_bird/scene.gltf");
   return <primitive object={scene} {...props} />;
 }
@@ -12,11 +14,24 @@ function Model(props: any) {
 const PhoenixModel = () => {
   return (
     <div className="w-full h-[300px] md:h-[500px] cursor-grab active:cursor-grabbing">
-      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+      <Canvas 
+        dpr={[1, 2]} 
+        shadows 
+        camera={{ fov: 45 }}
+        onCreated={({ gl }) => {
+          gl.toneMapping = THREE.AgXToneMapping;
+        }}
+      >
         <color attach="background" args={["transparent"]} />
         <Suspense fallback={null}>
-          <PresentationControls speed={1.5} global zoom={0.5} polar={[-0.1, Math.PI / 4]}>
-            <Stage environment="city">
+          <PresentationControls 
+            speed={1.5} 
+            global 
+            zoom={0.5} 
+            polar={[-0.1, Math.PI / 4]}
+            rotation={[0, -Math.PI / 4, 0]}
+          >
+            <Stage environment="city" intensity={0.5}>
               <Model scale={0.01} />
             </Stage>
           </PresentationControls>
